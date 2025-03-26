bbe57d5a31d06e8c5bdd71a9511ede99a21be54f
rafopm <rafopm@gmail.com>
2025-03-26 10:15:20 -0500

    add tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,23 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "dark-gray": "var(--color-dark-gray)",
+        black: "var(--color-black)",
+        white: "var(--color-white)",
+        "very-dark-gray": "var(--color-very-dark-gray)",
+        
+      },
+      fontFamily: {
+        primary: "var(--font-family-primary)",
+      },
+      fontSize: {
+        base: "var(--font-size-base)",
+      },
+      fontWeight: {
+        medium: "var(--font-weight-medium)",
+        semibold: "var(--font-weight-semibold)",
+        bold: "var(--font-weight-bold)",
+
       },
     },
   },
