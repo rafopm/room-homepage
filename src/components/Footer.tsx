33ac5ee99b62556845c7ce6c49a52fab5f0d66d7
rafopm <rafopm@gmail.com>
2025-04-12 22:06:48 +0000

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+
+  return (
+    <div className='flex justify-center items-center bg-black text-white font-bold'>
+     Coded by 
+     <a href="https://rafopm.github.io/rafopm/">&nbsp;Rafopm</a>
+    </div>
+  )
+}
+
+export default Footer
